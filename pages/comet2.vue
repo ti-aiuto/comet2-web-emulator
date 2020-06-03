01fa24d2fc3cad1cfa5983c5c5345b1ba2082862
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-03 20:23:23 +0900

    :pencil2: 実行機能実装中

--- pages/comet2.vue
+++ pages/comet2.vue
@@ -1,8 +1,64 @@
 <template>
-  
+  <v-container> </v-container>
 </template>
 <script lang="ts">
-export default {
-  
-}
+import Vue from "vue";
+import { Memory } from "ts-comet2-emulator/dist/memory";
+import { Register } from "ts-comet2-emulator/dist/register";
+import { Compiler } from "ts-comet2-emulator/dist/compiler";
+import { Machine } from "ts-comet2-emulator/dist/machine";
+import {
+  parseSource,
+  toWordHex,
+  memoryDebugInfo,
+} from "ts-comet2-emulator/dist/utils";
+import { IO } from "ts-comet2-emulator/dist/io";
+import { castle2Examples } from "../lib/examples";
+
+export default Vue.extend({
+  data() {
+    const io = new IO(
+      async function() {
+        return window.prompt("結果を入力") || "";
+      },
+      async function(value: string) {
+        alert(value);
+      }
+    );
+
+    const memory = new Memory();
+    const register = new Register();
+    const machine = new Machine(memory, register, io);
+
+    return {
+      io,
+      memory,
+      register,
+      machine,
+      source: castle2Examples[0],
+      logMessages: [] as string[]
+    };
+  },
+  methods: {
+    init() {
+      this.memory = new Memory();
+      this.register = new Register();
+      this.machine = new Machine(this.memory, this.register, this.io);
+    },
+    loadAndcompile() {
+      const firstAddress = 0;
+      const labelMap = {};
+      const compiler = new Compiler(
+        this.memory,
+        firstAddress,
+        this.source,
+        labelMap
+      );
+      const addrToSourceIndexMap = compiler.addrToSourceIndexMap();
+    },
+    log(message: string) {
+      this.logMessages.unshift(message);
+    }
+  },
+});
 </script>
