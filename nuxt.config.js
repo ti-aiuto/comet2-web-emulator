6dd94aa0b454399574aa8d933c4ba1da0e98ea10
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-03 21:04:08 +0900

    :heavy_plus_sign: metaタグ追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   buildModules: ["@nuxtjs/vuetify", "@nuxt/typescript-build"],
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+  ],
 };
