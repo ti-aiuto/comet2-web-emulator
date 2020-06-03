c454637af19f57fa237023cdb5372cbdce9f8062
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-03 19:46:48 +0900

    :pencil2: レイアウト作成

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-toolbar-title>ta.kasaki.info</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
